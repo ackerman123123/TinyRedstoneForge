365a08035d43b167ce1eb6a58db1237318fe5513
kyo <vladshahoiy9@gmail.com>
2026-07-03 22:09:34 +0300

    Add basic lamp signal logic

--- src/main/java/com/kyo/tinyredstoneforge/engine/SignalEngine.java
+++ src/main/java/com/kyo/tinyredstoneforge/engine/SignalEngine.java
@@ -1,5 +1,9 @@
 package com.kyo.tinyredstoneforge.engine;
 
+import com.kyo.tinyredstoneforge.component.PanelComponent;
+import com.kyo.tinyredstoneforge.component.input.LeverComponent;
+import com.kyo.tinyredstoneforge.component.output.LampComponent;
+import com.kyo.tinyredstoneforge.panel.PanelGrid;
 import com.kyo.tinyredstoneforge.signal.SignalGraph;
 import com.kyo.tinyredstoneforge.signal.SignalSolver;
 
@@ -21,7 +25,44 @@ public class SignalEngine {
             return;
         }
 
+        PanelGrid grid = context.blockEntity().engine().grid();
+
+        for (int y = 0; y < grid.height(); y++) {
+            for (int x = 0; x < grid.width(); x++) {
+                PanelComponent component = grid.getCell(x, y).getComponent();
+
+                if (component instanceof LampComponent lamp) {
+                    lamp.setInputSignal(0);
+                    applyNearbyLeverSignal(grid, x, y, lamp);
+                }
+            }
+        }
+
         solver.solve(graph);
         dirty = false;
+    }
+
+    private void applyNearbyLeverSignal(PanelGrid grid, int x, int y, LampComponent lamp) {
+        int[][] offsets = {
+                {1, 0},
+                {-1, 0},
+                {0, 1},
+                {0, -1}
+        };
+
+        for (int[] offset : offsets) {
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+
+            if (!grid.isInside(nx, ny)) {
+                continue;
+            }
+
+            PanelComponent neighbor = grid.getCell(nx, ny).getComponent();
+
+            if (neighbor instanceof LeverComponent lever) {
+                lamp.setInputSignal(Math.max(lamp.getInputSignal(), lever.getSignalStrength()));
+            }
+        }
     }
 }
