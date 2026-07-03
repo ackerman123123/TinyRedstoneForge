1ea247571e583724a0f8b958c170f2df314c6a83
kyo <vladshahoiy9@gmail.com>
2026-07-03 22:19:55 +0300

    Add wire signal propagation

--- src/main/java/com/kyo/tinyredstoneforge/engine/SignalEngine.java
+++ src/main/java/com/kyo/tinyredstoneforge/engine/SignalEngine.java
@@ -3,6 +3,7 @@ package com.kyo.tinyredstoneforge.engine;
 import com.kyo.tinyredstoneforge.component.PanelComponent;
 import com.kyo.tinyredstoneforge.component.input.LeverComponent;
 import com.kyo.tinyredstoneforge.component.output.LampComponent;
+import com.kyo.tinyredstoneforge.component.wire.WireComponent;
 import com.kyo.tinyredstoneforge.panel.PanelGrid;
 import com.kyo.tinyredstoneforge.signal.SignalGraph;
 import com.kyo.tinyredstoneforge.signal.SignalSolver;
@@ -27,22 +28,47 @@ public class SignalEngine {
 
         PanelGrid grid = context.blockEntity().engine().grid();
 
-        for (int y = 0; y < grid.height(); y++) {
-            for (int x = 0; x < grid.width(); x++) {
-                PanelComponent component = grid.getCell(x, y).getComponent();
-
-                if (component instanceof LampComponent lamp) {
-                    lamp.setInputSignal(0);
-                    applyNearbyLeverSignal(grid, x, y, lamp);
-                }
-            }
-        }
+        clearSignals(grid);
+        propagateLeverSignals(grid);
+        updateLamps(grid);
 
         solver.solve(graph);
         dirty = false;
     }
 
-    private void applyNearbyLeverSignal(PanelGrid grid, int x, int y, LampComponent lamp) {
+    private void clearSignals(PanelGrid grid) {
+        for (int y = 0; y < grid.height(); y++) {
+            for (int x = 0; x < grid.width(); x++) {
+                PanelComponent component = grid.getCell(x, y).getComponent();
+
+                if (component instanceof WireComponent wire) {
+                    wire.setSignalStrength(0);
+                }
+
+                if (component instanceof LampComponent lamp) {
+                    lamp.setInputSignal(0);
+                }
+            }
+        }
+    }
+
+    private void propagateLeverSignals(PanelGrid grid) {
+        for (int y = 0; y < grid.height(); y++) {
+            for (int x = 0; x < grid.width(); x++) {
+                PanelComponent component = grid.getCell(x, y).getComponent();
+
+                if (component instanceof LeverComponent lever && lever.isPowered()) {
+                    spreadSignal(grid, x, y, 15);
+                }
+            }
+        }
+    }
+
+    private void spreadSignal(PanelGrid grid, int x, int y, int signal) {
+        if (signal <= 0) {
+            return;
+        }
+
         int[][] offsets = {
                 {1, 0},
                 {-1, 0},
@@ -60,8 +86,57 @@ public class SignalEngine {
 
             PanelComponent neighbor = grid.getCell(nx, ny).getComponent();
 
-            if (neighbor instanceof LeverComponent lever) {
-                lamp.setInputSignal(Math.max(lamp.getInputSignal(), lever.getSignalStrength()));
+            if (neighbor instanceof WireComponent wire) {
+                if (signal - 1 > wire.getSignalStrength()) {
+                    wire.setSignalStrength(signal - 1);
+                    spreadSignal(grid, nx, ny, signal - 1);
+                }
+            }
+
+            if (neighbor instanceof LampComponent lamp) {
+                lamp.setInputSignal(Math.max(lamp.getInputSignal(), signal - 1));
+            }
+        }
+    }
+
+    private void updateLamps(PanelGrid grid) {
+        for (int y = 0; y < grid.height(); y++) {
+            for (int x = 0; x < grid.width(); x++) {
+                PanelComponent component = grid.getCell(x, y).getComponent();
+
+                if (!(component instanceof LampComponent lamp)) {
+                    continue;
+                }
+
+                int strongestSignal = lamp.getInputSignal();
+
+                int[][] offsets = {
+                        {1, 0},
+                        {-1, 0},
+                        {0, 1},
+                        {0, -1}
+                };
+
+                for (int[] offset : offsets) {
+                    int nx = x + offset[0];
+                    int ny = y + offset[1];
+
+                    if (!grid.isInside(nx, ny)) {
+                        continue;
+                    }
+
+                    PanelComponent neighbor = grid.getCell(nx, ny).getComponent();
+
+                    if (neighbor instanceof WireComponent wire) {
+                        strongestSignal = Math.max(strongestSignal, wire.getSignalStrength());
+                    }
+
+                    if (neighbor instanceof LeverComponent lever) {
+                        strongestSignal = Math.max(strongestSignal, lever.getSignalStrength());
+                    }
+                }
+
+                lamp.setInputSignal(strongestSignal);
             }
         }
     }
