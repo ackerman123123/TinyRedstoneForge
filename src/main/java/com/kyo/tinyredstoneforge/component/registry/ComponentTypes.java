e241780549b715434f2a9bc055fce6885cd11e2f
kyo <vladshahoiy9@gmail.com>
2026-07-03 21:26:57 +0300

    Add lever component

--- src/main/java/com/kyo/tinyredstoneforge/component/registry/ComponentTypes.java
+++ src/main/java/com/kyo/tinyredstoneforge/component/registry/ComponentTypes.java
@@ -1,22 +1,14 @@
 package com.kyo.tinyredstoneforge.component.registry;
 
-import com.kyo.tinyredstoneforge.component.PanelComponent;
+import com.kyo.tinyredstoneforge.component.input.LeverComponent;
 
 public final class ComponentTypes {
+    public static final ComponentType<LeverComponent> LEVER =
+            ComponentRegistry.register("lever", LeverComponent::new);
+
     public static void init() {
-        // Component types will be registered here.
+        // Forces static initialization.
     }
 
     private ComponentTypes() {}
-
-    public static final class EmptyComponent extends PanelComponent {
-        public EmptyComponent(int x, int y) {
-            super(x, y);
-        }
-
-        @Override
-        public String getId() {
-            return "empty";
-        }
-    }
 }
